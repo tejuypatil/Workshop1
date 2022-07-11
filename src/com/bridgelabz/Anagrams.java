710682a8b9e6c2778fbe14d554a6199ca4b2f924
tejuypatil <tejuypatil@gmail.com>
2022-07-11 14:57:55 +0530

    Added corrected program

--- src/com/bridgelabz/Anagrams.java
+++ src/com/bridgelabz/Anagrams.java
@@ -1,32 +1,43 @@
 package com.bridgelabz;
 
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Anagrams {
 
-    public static boolean checkAnagram(String s1, String s2) {
-        int n=(int)Math.round(Math.random());
+    public static boolean checkAnagram(String s1,String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        //if length of the both strings are not same then they cannot be anagram
+
+        if (n1 != n2)
         return false;
-    }
 
+        char[] charArray1 = s1.toCharArray();
+        char[] charArray2 = s2.toCharArray();
 
+        //sort both strings
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+        for (int i = 0; i < n1; i++)
+         if(charArray1[i] != charArray2[i])
+             return false;
+        return  true;
 
-    public static void main (String[]args){
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter the First String : ");
-            String s1 = scanner.nextLine();
-            System.out.print("Enter the second String : ");
-            String s2 = scanner.nextLine();
-            if (checkAnagram (s1, s2)) {
-                System.out.println(s1 + " and " + s2 + " are Anagrams");
-            } else
-                System.out.println(s1 + " and " + s2 + " are NOT Anagrams");
-            scanner.close();
+        }
+        public static void main (String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the First String : ");
+        String s1 = scanner.nextLine();
+        System.out.print("Enter the second String : ");
+        String s2 = scanner.nextLine();
+        if (checkAnagram (s1, s2)) {
+            System.out.println(s1 + " and " + s2 + " are Anagrams");
+        } else
+            System.out.println(s1 + " and " + s2 + " are NOT Anagrams");
+        scanner.close();
     }
 }
-
-
-
-
 
 
